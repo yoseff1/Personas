ce965e1b50d52f72d638ab4583a7e645ef41d902
yoseff <yoseffcajamarca@gmail.com>
2021-05-20 18:06:17 -0300

    trato de meter el constructor

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,5 +3,8 @@ package principal;
 import personas.Persona1;
 
 public class Principal {
+	public static void main(String[] args) {
+		Persona1 UsuarioA = new Persona1("Pedro", "Gutierrez", 95743254, 32, "1.84",82);
+	}
 
 }
