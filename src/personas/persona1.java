943e3287c2efa436c0df69f1c019a12411728a6c
yoseff <yoseffcajamarca@gmail.com>
2021-05-20 16:41:34 -0300

    inicio de personas

---
+++ src/personas/persona1.java
@@ -0,0 +1,12 @@
+package personas;
+
+public class persona1 {
+	private String marca; //yo puedo decidir si un parametro de una clase es publico o privado
+	private String modelo;
+	private int velocidadMaxima;
+	private int velocidadActual=0;
+	private boolean encendido;
+	private String patente;
+	private String color;
+
+}
